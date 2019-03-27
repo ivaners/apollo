93d26064dbe9e3459e3533a533e053fd34ab6267
ivan <longaiwen@gmail.com>
2019-03-27 19:54:27 +0800

    commit

--- apollo-biz/src/test/java/com/ctrip/framework/apollo/biz/AbstractUnitTest.java
+++
@@ -1,9 +0,0 @@
-package com.ctrip.framework.apollo.biz;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public abstract class AbstractUnitTest {
-
-}
