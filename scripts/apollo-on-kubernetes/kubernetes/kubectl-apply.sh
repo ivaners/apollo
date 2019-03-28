db6938ac50e4edb702c05ef6d84796b0445583f8
ivan <longaiwen@gmail.com>
2019-03-28 18:05:44 +0800

    commit

--- scripts/apollo-on-kubernetes/kubernetes/kubectl-apply.sh
+++ scripts/apollo-on-kubernetes/kubernetes/kubectl-apply.sh
@@ -2,8 +2,8 @@
 kubectl create namespace sre
 
 # dev-env
-kubectl apply -f service-apollo-config-server-dev.yaml --record && \
-kubectl apply -f service-apollo-admin-server-dev.yaml --record
+kubectl apply -f apollo-env-dev/service-apollo-config-server-dev.yaml --record && \
+kubectl apply -f apollo-env-dev/service-apollo-admin-server-dev.yaml --record
 
 # portal
 kubectl apply -f service-apollo-portal-server.yaml --record
